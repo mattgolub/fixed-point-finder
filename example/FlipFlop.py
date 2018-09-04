df1174ee6172859aa320071e39d5f79c68b7fe7e
Matt-Linux-Box <mgolub@stanford.edu>
2018-09-04 13:46:46 -0700

    Updated comments

--- example/FlipFlop.py
+++ example/FlipFlop.py
@@ -10,26 +10,16 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
-import numpy as np
-import numpy.random as npr
-import matplotlib.pyplot as plt
-
-import pdb
 import sys
-import os
-import shutil
-import ntpath # filesystem support for windows and linux
 
 PATH_TO_RECURRENT_WHISPERER = '../../recurrent-whisperer/'
 sys.path.insert(0, PATH_TO_RECURRENT_WHISPERER)
 from RecurrentWhisperer import RecurrentWhisperer
 
-from AdaptiveLearningRate import AdaptiveLearningRate
-from AdaptiveGradNormClip import AdaptiveGradNormClip
-from Hyperparameters import Hyperparameters
-
-# FULL COMMENTS COMING SOON.
+import tensorflow as tf
+import numpy as np
+import numpy.random as npr
+import matplotlib.pyplot as plt
 
 class FlipFlop(RecurrentWhisperer):
     ''' Class for training an RNN to implement an N-bit memory, a.k.a. "the
@@ -74,11 +64,11 @@ class FlipFlop(RecurrentWhisperer):
         return {
             'rnn_type': 'vanilla', # 'vanilla', 'gru' or 'lstm'
             'n_hidden': 24,
-            'data_hps':
-                {'n_batch':  128,
+            'data_hps': {
+                'n_batch':  128,
                 'n_time': 256,
                 'n_bits': 3,
-                'p_flip': 0.2},
+                'p_flip': 0.2}
             }
 
     @staticmethod
@@ -116,30 +106,36 @@ class FlipFlop(RecurrentWhisperer):
         n_output = n_inputs
 
         # Data handling
-        self.inputs_bxtxd = tf.placeholder(tf.float32, [n_batch, n_time, n_inputs])
-        self.output_bxtxd = tf.placeholder(tf.float32, [n_batch, n_time, n_output])
+        self.inputs_bxtxd = tf.placeholder(tf.float32,
+            [n_batch, n_time, n_inputs])
+        self.output_bxtxd = tf.placeholder(tf.float32,
+            [n_batch, n_time, n_output])
 
         # RNN
-        if hps.rnn_type=='vanilla':
+        if hps.rnn_type == 'vanilla':
             self.rnn_cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
-        elif hps.rnn_type=='gru':
+        elif hps.rnn_type == 'gru':
             self.rnn_cell = tf.nn.rnn_cell.GRUCell(n_hidden)
-        elif hps.rnn_type=='lstm':
+        elif hps.rnn_type == 'lstm':
             self.rnn_cell = tf.nn.rnn_cell.LSTMCell(n_hidden)
         else:
-            raise ValueError('Hyperparameter rnn_type must be one of [vanilla, gru, lstm] but was %s' % hps.rnn_type)
+            raise ValueError('Hyperparameter rnn_type must be one of '
+                '[vanilla, gru, lstm] but was %s' % hps.rnn_type)
 
         initial_state = self.rnn_cell.zero_state(n_batch, dtype=tf.float32)
-        self.hidden_bxtxd, _  = tf.nn.dynamic_rnn(self.rnn_cell, self.inputs_bxtxd, initial_state=initial_state)
+        self.hidden_bxtxd, _ = tf.nn.dynamic_rnn(self.rnn_cell,
+            self.inputs_bxtxd, initial_state=initial_state)
 
         # Readout from RNN
         np_W_out, np_b_out = self._np_init_weight_matrix(n_hidden, n_output)
-        self.W_out = tf.Variable(np_W_out,dtype=tf.float32)
-        self.b_out = tf.Variable(np_b_out,dtype=tf.float32)
-        self.pred_output_bxtxd = tf.tensordot(self.hidden_bxtxd, self.W_out,axes=1) + self.b_out
+        self.W_out = tf.Variable(np_W_out, dtype=tf.float32)
+        self.b_out = tf.Variable(np_b_out, dtype=tf.float32)
+        self.pred_output_bxtxd = tf.tensordot(self.hidden_bxtxd,
+            self.W_out, axes=1) + self.b_out
 
         # Loss
-        self.loss = tf.reduce_mean(tf.squared_difference(self.output_bxtxd, self.pred_output_bxtxd))
+        self.loss = tf.reduce_mean(
+            tf.squared_difference(self.output_bxtxd, self.pred_output_bxtxd))
 
     def _setup_saver(self):
         '''See docstring in RecurrentWhisperer.'''
@@ -155,7 +151,10 @@ class FlipFlop(RecurrentWhisperer):
         '''See docstring in RecurrentWhisperer.'''
 
 
-        ops_to_eval = [self.train_op, self.grad_global_norm, self.loss, self.merged_opt_summaries]
+        ops_to_eval = [self.train_op,
+            self.grad_global_norm,
+            self.loss,
+            self.merged_opt_summaries]
 
         feed_dict = dict()
         feed_dict[self.inputs_bxtxd] = batch_data['inputs']
@@ -163,7 +162,11 @@ class FlipFlop(RecurrentWhisperer):
         feed_dict[self.learning_rate] = self.adaptive_learning_rate()
         feed_dict[self.grad_norm_clip_val] = self.adaptive_grad_norm_clip()
 
-        [ev_train_op, ev_grad_global_norm, ev_loss, ev_merged_opt_summaries] = self.session.run(ops_to_eval, feed_dict=feed_dict)
+        [ev_train_op,
+            ev_grad_global_norm,
+            ev_loss,
+            ev_merged_opt_summaries] = \
+                self.session.run(ops_to_eval, feed_dict=feed_dict)
 
         self.writer.add_summary(ev_merged_opt_summaries, self._step())
 
@@ -176,9 +179,13 @@ class FlipFlop(RecurrentWhisperer):
 
         ops_to_eval = [self.hidden_bxtxd, self.pred_output_bxtxd]
         feed_dict = {self.inputs_bxtxd: batch_data['inputs']}
-        ev_hidden_bxtxd, ev_pred_output_bxtxd = self.session.run(ops_to_eval, feed_dict=feed_dict)
+        ev_hidden_bxtxd, ev_pred_output_bxtxd = \
+            self.session.run(ops_to_eval, feed_dict=feed_dict)
 
-        predictions = {'hidden': ev_hidden_bxtxd, 'output': ev_pred_output_bxtxd}
+        predictions = {
+            'hidden': ev_hidden_bxtxd,
+            'output': ev_pred_output_bxtxd
+            }
 
         return predictions
 
@@ -191,7 +198,9 @@ class FlipFlop(RecurrentWhisperer):
         return batch_data['inputs'].shape[0]
 
     def generate_flipflop_trials(self):
-        '''Generates synthetic data (i.e., ground truth trials) for the FlipFlop task. See comments following FlipFlop class definition for a description of the input-output relationship in the task.
+        '''Generates synthetic data (i.e., ground truth trials) for the
+        FlipFlop task. See comments following FlipFlop class definition for a
+        description of the input-output relationship in the task.
 
         Args:
             None.
@@ -199,9 +208,11 @@ class FlipFlop(RecurrentWhisperer):
         Returns:
             dict containing 'inputs' and 'outputs'.
 
-                'inputs': [n_batch x n_time x n_bits] numpy array containing input pulses.
+                'inputs': [n_batch x n_time x n_bits] numpy array containing
+                input pulses.
 
-                'outputs': [n_batch x n_time x n_bits] numpy array specifying the correct behavior of the FlipFlop memory device.
+                'outputs': [n_batch x n_time x n_bits] numpy array specifying
+                the correct behavior of the FlipFlop memory device.
         '''
 
         data_hps = self.hps.data_hps
@@ -214,10 +225,10 @@ class FlipFlop(RecurrentWhisperer):
         unsigned_inputs = npr.binomial(1, p_flip, [n_batch, n_time, n_bits])
 
         # Ensure every trial is initialized with a pulse at time 0
-        unsigned_inputs[:,0,:] = 1
+        unsigned_inputs[:, 0, :] = 1
 
         # Generate random signs {-1, +1}
-        random_signs = 2*npr.binomial(1,0.5,[n_batch, n_time, n_bits]) - 1
+        random_signs = 2*npr.binomial(1, 0.5, [n_batch, n_time, n_bits]) - 1
 
         # Apply random signs to input pulses
         inputs = np.multiply(unsigned_inputs, random_signs)
@@ -228,15 +239,17 @@ class FlipFlop(RecurrentWhisperer):
         # Update inputs (zero-out random start holds) & compute output
         for trial_idx in range(n_batch):
             for bit_idx in range(n_bits):
-                input =  np.squeeze(inputs[trial_idx,:,bit_idx])
-                t_flip = np.where(input!=0)
+                _input = np.squeeze(inputs[trial_idx, :, bit_idx])
+                t_flip = np.where(_input != 0)
                 for flip_idx in range(np.size(t_flip)):
                     # Get the time of the next flip
                     t_flip_i = t_flip[0][flip_idx]
 
-                    # Set the output to the sign of the flip for the remainder of the trial
-                    # Future flips will overwrite future output
-                    output[trial_idx,t_flip_i:,bit_idx] = inputs[trial_idx,t_flip_i,bit_idx]
+                    '''Set the output to the sign of the flip for the
+                    remainder of the trial. Future flips will overwrite future
+                    output'''
+                    output[trial_idx, t_flip_i:, bit_idx] = \
+                        inputs[trial_idx, t_flip_i, bit_idx]
 
         return {'inputs': inputs, 'output': output}
 
@@ -244,13 +257,14 @@ class FlipFlop(RecurrentWhisperer):
         '''See docstring in RecurrentWhisperer.'''
         self.fig = plt.figure()
 
-    def _update_visualization(self, train_data = None, valid_data = None):
+    def _update_visualization(self, train_data=None, valid_data=None):
         '''See docstring in RecurrentWhisperer.'''
         data = self.generate_flipflop_trials()
         self.plot_trials(data)
 
     def plot_trials(self, data):
-        '''Plots example trials, complete with input pulses, correct outputs, and RNN-predicted outputs.
+        '''Plots example trials, complete with input pulses, correct outputs,
+        and RNN-predicted outputs.
 
         Args:
             data: dict as returned by generate_flipflop_trials.
@@ -271,14 +285,17 @@ class FlipFlop(RecurrentWhisperer):
         n_plot = np.min([self.hps.n_trials_plot, n_batch])
 
         for trial_idx in range(n_plot):
-            ax = plt.subplot(n_plot,1,trial_idx+1)
+            ax = plt.subplot(n_plot, 1, trial_idx+1)
             for bit_idx in range(n_bits):
                 vertical_offset = 2.5*bit_idx
-                ax.plot(vertical_offset + inputs[trial_idx,:,bit_idx],color='purple')
-                ax.plot(vertical_offset + output[trial_idx,:,bit_idx],color='green')
-                ax.plot(vertical_offset + pred_output[trial_idx,:,bit_idx],color='cyan',linestyle='--')
+                ax.plot(vertical_offset + inputs[trial_idx, :, bit_idx],
+                    color='purple')
+                ax.plot(vertical_offset + output[trial_idx, :, bit_idx],
+                    color='green')
+                ax.plot(vertical_offset + pred_output[trial_idx, :, bit_idx],
+                    color='cyan', linestyle='--')
             plt.yticks([])
-            if trial_idx<(n_plot-1):
+            if trial_idx < (n_plot-1):
                 plt.xticks([])
 
         plt.ion()
